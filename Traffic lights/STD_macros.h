4bd0a4fb6b2204187e4ec79dd6e936eb641c198a
sherif osama <sherif.os.abdelkareem@gmail.com>
2024-09-30 10:50:25 +0300

    Update STD_macros.h

--- Traffic lights/STD_macros.h
+++ Traffic lights/STD_macros.h
@@ -10,7 +10,7 @@
 #define STD_MACROS_H_
 #define SET_BIT(reg , bit)	reg|=(1<<bit)
 #define CLR_BIT(reg , bit)	reg&=(~(1<<bit)) 
-#define READ_BIT(reg , bit)  (reg>>bit)&1                         //return 1 or 0 
+#define READ_BIT(reg , bit)    (reg>>bit)&1                         //return 1 or 0 
 #define TOGG_BIT(reg , bit)	reg^=(1<<bit)
 
 #endif /* STD_MACROS_H_ */
